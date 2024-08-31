dbf458b01375c30e47362b8a61bd020564d39a81
PeetrS <hinode2@gmail.com>
2024-08-31 10:35:27 +0200

    basic structure

---
+++ my_first_app/lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kalkulačka',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Moje kalkulačka'),
+        ),
+        body: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
